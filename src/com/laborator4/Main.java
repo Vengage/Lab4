fb0d4b7b38514dcae2db2ff969ea0bd8a8cbc969
Vengage <vengage_92@yahoo.com>
2014-11-10 10:15:13 +0200

    First commit

---
+++ src/com/laborator4/Main.java
@@ -0,0 +1,14 @@
+package com.laborator4;
+
+/**
+ * Created by venga_000 on 11/10/2014.
+ *
+ * This is the Main class. It holds the role of the client.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
